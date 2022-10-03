4d085b6fe0af1dc2eb13a8ea973122c20a313ad6
lordW1lly <sebastianengelstajn@gmail.com>
2022-10-03 17:05:41 -0300

    Update NavBarBTS.jsx

--- PI-Dogs-main/client/src/components/NavBar/NavBarBTS.jsx
+++ PI-Dogs-main/client/src/components/NavBar/NavBarBTS.jsx
@@ -19,7 +19,9 @@ export function NavBar() {
 
             <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">ThePerritosApp</a>
+                    <Link class='noDeco' exact to="/dogs" onClick={() => dispatch(getDogs())} >
+                        <a class="navbar-brand">ThePerritosApp</a>
+                    </Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -49,7 +51,7 @@ export function NavBar() {
                             </ul>
                         </li> */}
 
-                           {/*  <li class="nav-item">
+                            {/*  <li class="nav-item">
                                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disableddddd</a>
                             </li> */}
                             <li class="nav-item">
